8f4a5314b3668c4e211a9e9d6de75e16f392586a
nasirmovlamov@gmail.com <movlamovnasir2000@gmail.com>
2021-12-24 16:25:06 +0400

    size adjust

--- pages/index.tsx
+++ pages/index.tsx
@@ -97,9 +97,6 @@ const HomePage:FC = () => {
 
       <footer className={styles.footer}>
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
         >
           <p>Powered by</p>
           
